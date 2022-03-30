07408244d92cf67568efae120567a1549fb3c042
AHMED <essabarahmedsds@gmail.com>
2022-03-30 15:27:13 +0200

    initialiser  grandeurs

--- utilsLaris.py
+++ utilsLaris.py
@@ -89,6 +89,8 @@ def outliersToNan(data):
 
 
 def seperateGrandeurs(df,grandeurs = {"temperature": [],"co2": [],"humidity": [],"sound": [],"tvoc": []}):
+    for grandeurs_key in  grandeurs:
+        grandeurs[grandeurs_key] = []        
     colonnesName=df.columns
     #grandeurs = {"temperature": [],"co2": [],"humidity": [],"sound": [],"tvoc": []}
     for grandeurs_key in  grandeurs:
@@ -96,6 +98,6 @@ def seperateGrandeurs(df,grandeurs = {"temperature": [],"co2": [],"humidity": []
             if name.find(grandeurs_key)==0:
                 grandeurs[grandeurs_key].append(name)
                 
-    return grandeurs    
+    return grandeurs     
     
     

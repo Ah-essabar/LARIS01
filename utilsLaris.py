ffd3de680d377e015b0e93fedcb4caad26cc6290
es-sabar <essabarahmedsds@gmail.com>
2022-04-04 09:47:28 +0200

    added
    df_column_uniquify

--- utilsLaris.py
+++ utilsLaris.py
@@ -108,3 +108,17 @@ def seperateGrandeurs(df,grandeurs = ["temperature","co2","humidity","sound","tv
                 grandeursTemp[grandeursTemp_key].append(name)
                 
     return grandeursTemp
+
+
+def df_column_uniquify(df):
+    df_columns = df.columns
+    new_columns = []
+    for item in df_columns:
+        counter = 0
+        newitem = item
+        while newitem in new_columns:
+            counter += 1
+            newitem = "{}_{}".format(item, counter)
+        new_columns.append(newitem)
+    df.columns = new_columns
+    return df
